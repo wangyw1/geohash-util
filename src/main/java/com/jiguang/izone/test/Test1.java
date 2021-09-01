4b9bb2bc8b4fcde5691ac3ebe9b2daf90892ed85
wangyw1 <wangyw1@jiguang.cn>
2021-09-01 11:39:54 +0800

    自定义网格

---
+++ src/main/java/com/jiguang/izone/test/Test1.java
@@ -0,0 +1,28 @@
+package com.jiguang.izone.test;
+
+import com.jiguang.izone.model.Mode;
+import com.jiguang.izone.util.CustomGrid;
+import com.jiguang.izone.util.GeohashPolyUtil;
+import com.vividsolutions.jts.geom.Geometry;
+
+import java.util.Set;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+
+        CustomGrid customGrid = new CustomGrid(120, 30, 500, 500);
+        double area = customGrid.getArea();
+        String centerPoint = customGrid.getCenterPoint();
+        String s = customGrid.toBase32();
+        System.out.println("area:"+area);
+        System.out.println("centerPoint:"+centerPoint);
+
+        CustomGrid customGrid1 = CustomGrid.fromGridString(s);
+
+        System.out.println(customGrid1.getArea());
+        System.out.println(customGrid1.getCenterPoint());
+
+        System.out.println();
+    }
+}
